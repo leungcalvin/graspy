103d3a54ff72f987b8fbd1f2a41ec41f41f3b67e
Calvin Leung <calvinl@mit.edu>
2020-05-30 16:24:43 -0700

    changed some dirs and write_csf

--- calc-scripts/safronova.py
+++ calc-scripts/safronova.py
@@ -1,13 +1,17 @@
 from grasp import *
 #This file implements the GRASP calculation documented here:
 
-dir_6s2 = '/home/calvin/graspy/safronova/est_6s2'
+dir_6s = '/home/calvin/graspy/calc-outputs/safronova/est_6s2'
+dir_6p = '/home/calvin/graspy/calc-outputs/safronova/est_6p'
+dir_5d = '/home/calvin/graspy/calc-outputs/safronova/est_5d'
+dir_ci = '/home/calvin/graspy/calc-outputs/safronova/ci'
+
 clistordering = ['1s','2s','2p','3s','3p','3d','4s','4p','4d','5s','5p','4f','5d','6s','6p','5f','6d','7p','8s']
-initialize(workdir=dir_6s2,clist=clistordering)
+initialize(workdir=dir_6s,clist=clistordering)
 mr_6s2 = Rcsfgenerate('Xe',
             ['4f(14,*)6s(2,*)'],
             activeset=[6,5,4,4],
-            jlower=0,jhigher=0,exc=0)
+            jlower=0,jhigher=0,exc=0,write_csf='rcsfmr.inp')
 
 indices_6s2 = [[1]]
 
@@ -19,11 +23,11 @@ est6s2 =[
                      fallback='Thomas-Fermi'),
         Rmcdhf(indices_6s2,orbs=['6s'],specorbs=['*'],weightingmethod='Standard',runs=1000),
         Rwfnestimate(orbdict=
-            {'*':os.path.join(dir_6s2,'rwfn.out')},
+            {'*':os.path.join(dir_6s,'rwfn.out')},
                      fallback='Thomas-Fermi'),
         Rmcdhf(indices_6s2,orbs=['4f*'],specorbs=['*'],weightingmethod='Standard',runs=1000),
         Rwfnestimate(orbdict=
-            {'*':os.path.join(dir_6s2,'rwfn.out')},
+            {'*':os.path.join(dir_6s,'rwfn.out')},
                      fallback='Thomas-Fermi'),
         Rmcdhf(indices_6s2,orbs=['*'],specorbs=['*'],weightingmethod='Standard',runs=1000),
         ]
@@ -31,18 +35,17 @@ est6s2 =[
 #
 # Estimate 6p orbitals using 4f13 6s2 6p configurations
 #
-dir_6p = '/home/calvin/graspy/safronova/est_6p'
 initialize(workdir=dir_6p,clist=clistordering)
 mr_6p = Rcsfgenerate('Xe',
             ['4f(13,*)6s(2,*)6p(1,*)'],
             activeset=[6,6,4,4],
-            jlower=0,jhigher=10,exc=0)
+            jlower=0,jhigher=10,exc=0,write_csf = 'rcsfmr.inp')
 indices_6p = [[1],[1,2,3],[1,2,3,4],[1,2,3],[1]]
 est6p =[
         Rnucleus(Z=70,A=172,neutralMass=171.936378,I=0,NDM=0,NQM=0),
         Rangular(),
         Rwfnestimate(orbdict=
-            {'*':os.path.join(dir_6s2,'rwfn.out')},
+            {'*':os.path.join(dir_6s,'rwfn.out')},
                      fallback='Thomas-Fermi'),
         Rmcdhf(indices_6p,orbs=['6p*'],specorbs=['*'],weightingmethod='Standard',runs=1000)
         ]
@@ -51,42 +54,40 @@ est6p =[
 #
 # Estimate 5d orbitals using 4f13 6s2 5d configurations
 #
-dir_5d = '/home/calvin/graspy/safronova/est_5d'
 initialize(workdir=dir_5d,clist=clistordering)
 mr_5d = Rcsfgenerate('Xe',
             ['4f(13,*)5d(1,*)6s(2,*)'],
             activeset=[6,5,5,4],
-            jlower=0,jhigher=14,exc=0)
+            jlower=0,jhigher=14,exc=0,write_csf = 'rcsfmr.inp')
 indices_5d = [[1],[1,2,3],[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3],[1]]
 est5d =[
         Rnucleus(Z=70,A=172,neutralMass=171.936378,I=0,NDM=0,NQM=0),
         Rangular(),
         Rwfnestimate(orbdict=
-            {'*':os.path.join(dir_6s2,'rwfn.out')},
+            {'*':os.path.join(dir_6s,'rwfn.out')},
                      fallback='Thomas-Fermi'),
         Rmcdhf(indices_5d,orbs=['5d*'],specorbs=['*'],weightingmethod='Standard',runs=1000)
         ]
 
 
 
-dir_ci = '/home/calvin/graspy/safronova/ci'
 initialize(workdir=dir_ci,clist=clistordering)
-zeros_6s2 = Rcsfgenerate('Xe',['4f(14,*)6s(2,*)'],activeset=[6,6,6,5],jlower=0,jhigher=2,exc=0)
-zeros_6s5d= Rcsfgenerate('Xe',['4f(14,*)5d(1,*)6s(1,*)'],activeset=[6,5,5,4],jlower=0,jhigher=8,exc=0)
+zeros_6s2 = Rcsfgenerate('Xe',['4f(14,*)6s(2,*)'],activeset=[6,6,6,5],jlower=0,jhigher=2,exc=0,write_csf='rcsfmr.inp')
+zeros_6s5d= Rcsfgenerate('Xe',['4f(14,*)5d(1,*)6s(1,*)'],activeset=[6,5,5,4],jlower=0,jhigher=8,exc=0,write_csf='rcsfmr.inp')
 zeros_even = zeros_6s2 + zeros_6s5d
 
-zeros_6s6p= Rcsfgenerate('Xe',['4f(14,*)6s(1,*)6p(1,*)',],activeset=[6,6,4,4],jlower=0,jhigher=4,exc=0)
-zeros_4f5d= Rcsfgenerate('Xe',['4f(13,*)5d(1,*)6s(2,*)',],activeset=[6,5,5,4],jlower=0,jhigher=14,exc=0)
+zeros_6s6p= Rcsfgenerate('Xe',['4f(14,*)6s(1,*)6p(1,*)',],activeset=[6,6,4,4],jlower=0,jhigher=4,exc=0,write_csf='rcsfmr.inp')
+zeros_4f5d= Rcsfgenerate('Xe',['4f(13,*)5d(1,*)6s(2,*)',],activeset=[6,5,5,4],jlower=0,jhigher=14,exc=0,write_csf='rcsfmr.inp')
 zeros_odd = zeros_6s6p + zeros_4f5d
 
 multireference = zeros_odd + zeros_even
 
-singles_6s2 = Rcsfgenerate('Xe',['4f(14,*)6s(2,*)'],activeset=[7,7,6,5],jlower=0,jhigher=2,exc=2)
-singles_4f5d = Rcsfgenerate('Xe',['4f(14,*)5d(1,*)6s(1,*)'],activeset=[7,7,6,5],jlower=0,jhigher=10,exc=2)
+singles_6s2 = Rcsfgenerate('Xe',['4f(14,*)6s(2,*)'],activeset=[7,7,6,5],jlower=0,jhigher=2,exc=2,write_csf='rcsfmr.inp')
+singles_4f5d = Rcsfgenerate('Xe',['4f(14,*)5d(1,*)6s(1,*)'],activeset=[7,7,6,5],jlower=0,jhigher=10,exc=2,write_csf='rcsfmr.inp')
 singles_even = singles_6s2 + singles_4f5d
 
-singles_6s6p= Rcsfgenerate('Xe',['4f(14,*)6s(1,*)6p(1,*)',],activeset=[7,7,6,5],jlower=0,jhigher=6 ,exc=2)
-singles_4f5d= Rcsfgenerate('Xe',['4f(13,*)5d(1,*)6s(2,*)',],activeset=[7,7,6,5],jlower=0,jhigher=16,exc=2)
+singles_6s6p= Rcsfgenerate('Xe',['4f(14,*)6s(1,*)6p(1,*)',],activeset=[7,7,6,5],jlower=0,jhigher=6 ,exc=2,write_csf='rcsfmr.inp')
+singles_4f5d= Rcsfgenerate('Xe',['4f(13,*)5d(1,*)6s(2,*)',],activeset=[7,7,6,5],jlower=0,jhigher=16,exc=2,write_csf='rcsfmr.inp')
 singles_odd = singles_6s6p + singles_4f5d
 singles_exp = singles_even + singles_odd
 
@@ -103,7 +104,7 @@ ci_run = [
          Rangular(),
          Rwfnestimate(orbdict={'6p*':os.path.join(dir_6p, 'rwfn.out'),
                                '5d*':os.path.join(dir_5d, 'rwfn.out'),
-                               '*' :os.path.join(dir_6s2,'rwfn.out')},
+                               '*' :os.path.join(dir_6s,'rwfn.out')},
                                fallback='Thomas-Fermi'),
          Rmcdhf(indices_mr,orbs=['5f*,6d*,7s,7p*'],specorbs=[''],weightingmethod='Standard',runs=1000),
          Rsave('safronova'),
@@ -126,14 +127,14 @@ readout = [
         Rlevels(calcname='safronova')
         ]
 
-mr_6s2.execute(workdir = dir_6s2, writeMR=True) # define the multireference
-[cmd.execute(workdir = dir_6s2) for cmd in est6s2]
-mr_5d.execute(workdir = dir_5d, writeMR=True) # define the multireference
+mr_6s2.execute(workdir = dir_6s) # define the multireference
+[cmd.execute(workdir = dir_6s) for cmd in est6s2]
+mr_5d.execute(workdir = dir_5d) # define the multireference
 [cmd.execute(workdir = dir_5d) for cmd in est5d]
-mr_6p.execute(workdir = dir_6p, writeMR=True) # define the multireference
+mr_6p.execute(workdir = dir_6p) # define the multireference
 [cmd.execute(workdir = dir_6p) for cmd in est6p]
 
-multireference.execute(workdir=dir_ci,writeMR=True)
+multireference.execute(workdir=dir_ci)
 [cmd.execute(workdir=dir_ci) for cmd in ci_run]
 [cmd.execute(workdir=dir_ci) for cmd in readout]
 #        Rcsfinteract('Dirac-Coulomb'),
