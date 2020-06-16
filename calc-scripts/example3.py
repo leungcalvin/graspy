16aab8167affa03e5802d0502f8b5e14539ca6ff
laasyamb <66223527+laasyamb@users.noreply.github.com>
2020-06-16 13:37:16 -0700

    Update example3.py
    it runs without errors, but the numbers of the outputs are slightly off from the manual

--- calc-scripts/example3.py
+++ calc-scripts/example3.py
@@ -1,59 +1,56 @@
-from grasp import *
-import sys
-#
-# This implements the GRASP 2018 calculation for 1s2 2s 2S and 1s2 2p 2P in Li I, as found in the GRASP 2018 manual, using the GRASPy interface.
-#
-testdir = './calc-outputs/example3'
+from graspy.grasp import *
+
+testdir = './calc-outputs/example3prime'
 initialize(workdir=testdir)
 
-# 1) Generate a multireference consisting of the 2s2 2p3 and 2p5 configurations
-ref_2s = Rcsfgenerate(core='None',ordering = 'Default',
-            csflist=['2s(2,i)2p(3,i)'],
-            activeset=[2],
-            jlower=1,jhigher=1,exc=0,write_csf= 'rcsfmr.inp')
-print("laasya - ref_2s=",ref_2s)
-ref_2p = Rcsfgenerate(core='None',ordering = 'Default',
-            csflist=['2p(5,i)'],
-            activeset = [2],
-            jlower=1,jhigher=3,exc=0,write_csf= 'rcsfmr.inp')
+ref_a = Rcsfgenerate(core='None',ordering = 'Default',
+            csflist=['1s(2,i)2s(2,i)2p(3,i)'],
+            activeset=[2,2],
+            jlower=1,jhigher=5,exc=0,write_csf= 'rcsfmr.inp')
+ref_b = Rcsfgenerate(core='None',ordering = 'Default',
+            csflist=['1s(2,i)2p(5,i)'],
+            activeset = [2,2],
+            jlower=1,jhigher=5,exc=0,write_csf= 'rcsfmr.inp')
 
 # The three states which make up the multireference can be added together with arithmetic.
-mr = ref_2s + ref_2p
-mr.execute(workdir = testdir) #the execute() method performs the actual call to GRASP 2018.
-#QUIT HERE, RCSF.INP AND RCSFMR.INP ARE BLANK, MUST DEBUG BEFORE PROCEEDING
-sys.exit(1)
-# 2) Perform a SCF procedure to solve for the 2s,2p orbitals.
+mr = ref_a + ref_b
+mr.execute(workdir = testdir)
+
 MR_DHF =[
         Rnucleus(Z=14,A=28,neutralMass=27.9769271,I=1,NDM=1,NQM=1),
         Rangular(),
-        Rwfnestimate(orbdict = None, fallback='Thomas-Fermi'),
-        Rmcdhf([[1,2],[1, 2, 3, 4],[1]],orbs = ['*'],specorbs = ['*'], runs = 100, weightingmethod = 'Standard'),
+        Rwfnestimate(orbdict = None, fallback='Screened Hydrogenic'),
+        Rmcdhf([[1,2],[1,2,3,4],[1]],orbs = ['*'],specorbs = ['*'], runs = 100, weightingmethod = 'Standard'),
         Rsave('2s22p3_2p5_DF')
         ]
 [cmd.execute(workdir = testdir) for cmd in MR_DHF]
 
-
-# 3) Generate a CAS expansion from the 2S configuration.
-CAS_2S_exp = Rcsfgenerate(core='None',ordering = 'Default',
-            csflist=['2s(2,*)'],
+CAS_a = Rcsfgenerate(core='None',ordering = 'Default',
+            csflist=['1s(2,*)2s(2,*)2p(3,*)'],
             activeset=[3,3,3],
-            jlower=1,jhigher=1,exc=3,write_csf = 'rcsf.inp')
+            jlower=1,jhigher=5,exc=2,write_csf = 'rcsf.inp')
+            
+CAS_b = Rcsfgenerate(core='None',ordering = 'Default',
+    csflist=['1s(2,*)2p(5,*)'],
+    activeset=[3,3,3],
+    jlower=1,jhigher=5,exc=2,write_csf = 'rcsf.inp')
+
+CAS_2S_exp = CAS_a + CAS_b
 CAS_2S_exp.execute(workdir = testdir)
 
-# 4) Solve for the n=3 correlation orbitals, using orbitals generated from 2s22p3_2p5_DF.w above.
-indices_2S = [[1]]
 CAS_2S = [
         Rangular(),
-        Rwfnestimate(orbdict = {'*':'2s22p3_2p5_DF'},fallback = 'Thomas-Fermi'),
-        Rmcdhf(asfidx = indices_2S,
+        Rwfnestimate(orbdict = {'*':'2s22p3_2p5_DF.w'},fallback = 'Screened Hydrogenic'),
+        Rmcdhf([[1,2],[1,2,3,4],[1]],
             orbs = ['3*'],
             specorbs = [' '],
             runs = 100, weightingmethod = 'Standard'),
         Rsave('2s22p3_2p5_3')
         ]
+        
 [cmd.execute(workdir = testdir) for cmd in CAS_2S]
+indices_try=[[1,2],[1,2,3,4],[1]]
 
-# 5) Perform CI on the 2S expansion.
 CI_2S = [
         Rci(calcname='2s22p3_2p5_3',
                 includetransverse=True,
@@ -64,50 +61,14 @@ CI_2S = [
                 includesms = False,
                 estselfenergy= True,
                 largestn = 3,
-                asfidx = indices_2S),
+                asfidx = indices_try),
         JJtoLSJ(calcname= '2s22p3_2p5_3',useCI = True, unique = True),
         ]
 [cmd.execute(workdir = testdir) for cmd in CI_2S]
 
-# 6) Generate a CAS expansion from the 2P configuration.
-CAS_2P_exp = Rcsfgenerate(core='None',ordering = 'Default',
-            csflist=['2p(5,*)'],
-            activeset=[3,3,3],
-            jlower=1,jhigher=3,exc=3)
-
-CAS_2P_exp.execute(workdir = testdir)
-
-# 7) Solve for the n=3 correlation orbitals, using orbitals generated from 2s_2p_DF.w above.
-indices_2P = [[1],[1]]
-CAS_2P = [
-        Rangular(),
-        Rwfnestimate(orbdict = {'*':'2s_2p_DF.w'},fallback = 'Thomas-Fermi'),
-        Rmcdhf(asfidx = indices_2P,orbs = ['3*'],specorbs = [' '],runs = 100, weightingmethod = 'Standard'),
-        Rsave('2p5_3')
-        ]
-[cmd.execute(workdir = testdir) for cmd in CAS_2P]
-
-# 8) Perform CI on the 2P expansion.
-CI_2P = [
-        Rci(calcname='2p5_3',
-                includetransverse=True,
-                modifyfreq=True,
-                scalefactor='1.d-6',
-                includevacpol = True,
-                includenms= False,
-                includesms = False,
-                estselfenergy= True,
-                largestn = 3,
-                asfidx = indices_2P),
-        JJtoLSJ(calcname= '2p5_3',useCI = True, unique = True),
-        ]
-[cmd.execute(workdir = testdir) for cmd in CI_2P]
-
-# 9) Calculate transitions.
 transitions_2P = [
-        Rhfs(calcname = '2p5_3',useCI=True),
-        Rbiotransform(useCI=True,calcname_initial = '2s22p3_2p5_3',calcname_final = '2p5_3', transform_all = True),
-        Rtransition(useCI=True,calcname_initial = '2s22p3_2p5_3',calcname_final = '2p5_3',transition_spec = ['E1'])]
+        Rhfs(calcname = '2s22p3_2p5_3',useCI=True),
+        Rbiotransform(useCI=True,calcname_initial = '2s22p3_2p5_3',calcname_final = '2s22p3_2p5_3', transform_all = True),
+        Rtransition(useCI=True,calcname_initial = '2s22p3_2p5_3',calcname_final = '2s22p3_2p5_3',transition_spec = ['E1'])]
 
 [cmd.execute(workdir = testdir) for cmd in transitions_2P]
-
