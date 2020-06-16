2bedaac6e5f7813c77e1ae48c732bbae4706663b
Calvin Leung <calvinl@mit.edu>
2020-06-15 23:06:28 -0700

    changed import

--- calc-scripts/safronova.py
+++ calc-scripts/safronova.py
@@ -1,4 +1,4 @@
-from grasp import *
+from graspy.grasp import *
 #This file implements the GRASP calculation documented here:
 
 dir_6s = '/home/calvin/graspy/calc-outputs/safronova/est_6s2'
@@ -108,7 +108,7 @@ ci_run = [
                                fallback='Thomas-Fermi'),
          Rmcdhf(indices_mr,orbs=['5f*,6d*,7s,7p*'],specorbs=[''],weightingmethod='Standard',runs=1000),
          Rsave('safronova'),
-         Rci(files='safronova',
+         Rci(calcname='safronova',
                  includetransverse=True,
                  modifyfreq=True,
                  scalefactor='1.d-6',
