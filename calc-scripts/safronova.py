84802e3ce845f95da48c204011fb9551e883fffa
Calvin Leung <calvinl@mit.edu>
2020-07-04 17:46:28 -0700

    fixed n = 4 core, running [4,5,6] (x) n=7,8,9

--- calc-scripts/safronova.py
+++ calc-scripts/safronova.py
@@ -54,12 +54,19 @@ def full_calculation(calc_dir,active_set,exc,n_open):
 #
 # Estimate 5d orbitals using 4f13 6s2 5d configurations
 #
-    def estimate_5d():
-        mr_5d = Rcsfgenerate('Xe',
-                    ['4f(13,*)5d(1,*)6s(2,*)'],
-                    active_set=[6,5,5,4],
-                    jlower=0,jhigher=14,exc=0,write_csf = 'rcsfmr.inp')
-        indices_5d = [[1],[1,2,3],[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3],[1]]
+    def estimate_5d(which_csf):
+        if which_csf == 'odd':
+            mr_5d = Rcsfgenerate('Xe',
+                        ['4f(13,*)5d(1,*)6s(2,*)'],
+                        active_set=[6,5,5,4],
+                        jlower=0,jhigher=14,exc=0,write_csf = 'rcsfmr.inp')
+            indices_5d = [[1],[1,2,3],[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3],[1]]
+        if which_csf == 'even':
+            mr_5d = Rcsfgenerate('Xe',
+                        ['4f(14,*)5d(1,*)6s(1,*)'],
+                        active_set=[6,5,5,4],
+                        jlower=2,jhigher=6,exc=0,write_csf = 'rcsfmr.inp')
+            indices_5d = [[1],[1,2],[1]] # just the singlet and triplet
         est5d =[
                 mr_5d,
                 Rnucleus(Z=70,A=172,neutralMass=171.936378,I=0,NDM=0,NQM=0),
@@ -67,26 +74,10 @@ def full_calculation(calc_dir,active_set,exc,n_open):
                 Rwfnestimate(orbdict=
                     {'*':os.path.join(dir_6s,'rwfn.out')},
                              fallback='Thomas-Fermi'),
-                Rmcdhf(indices_5d,orbs=['5d*'],specorbs=['*'],weighting_method='Standard',runs=1000)
+                Rmcdhf(indices_5d,orbs=['5d*'],specorbs=['*'],weighting_method='Standard',runs=1000,integration_method = 3)
                 ]
         return est5d
 
-    def estimate_5d_closed():
-        mr_5d = Rcsfgenerate('Xe',
-                    ['4f(14,*)5d(1,*)6s(1,*)'],
-                    active_set=[6,5,5,4],
-                    jlower=2,jhigher=6,exc=0,write_csf = 'rcsfmr.inp')
-        indices_5d = [[1],[1,2],[1]] # just the singlet and triplet
-        est5d =[
-                mr_5d,
-                Rnucleus(Z=70,A=172,neutralMass=171.936378,I=0,NDM=0,NQM=0),
-                Rangular(),
-                Rwfnestimate(orbdict=
-                    {'*':os.path.join(dir_6s,'rwfn.out')},
-                             fallback='Thomas-Fermi'),
-                Rmcdhf(indices_5d,orbs=['5d*'],specorbs=['*'],weighting_method='Standard',runs=1000)
-                ]
-        return est5d
    ##################
    #
    #
@@ -95,18 +86,30 @@ def full_calculation(calc_dir,active_set,exc,n_open):
    ##################
     mr_6s2.execute(workdir = dir_6s) # define the multireference
     [cmd.execute(workdir = dir_6s) for cmd in est6s2]
+    input('6s OK?')
     mr_6p.execute(workdir = dir_6p) # define the multireference
     [cmd.execute(workdir = dir_6p) for cmd in est6p]
+    input('6p OK?')
     initialize(workdir=dir_5d,clist=clistordering)
-    [cmd.execute(workdir = dir_5d) for cmd in estimate_5d_closed()]
+    [cmd.execute(workdir = dir_5d) for cmd in estimate_5d('odd')]
+    input('5d OK?')
 
+   ##################
+   #
+   #
+   # CI Calculations
+   #
+   ##################
+
+    AR_PREFIX = '3d(10,c)4s(2,*)4p(6,*)4d(10,*)5s(2,*)5p(6,*)'
     def gen_multireference():
-        zeros_6s2 = Rcsfgenerate('Kr',['4d(10,i)5s(2,i)5p(6,i)4f(14,*)6s(2,*)'],active_set=[6,6,6,5],jlower=0,jhigher=2,exc=0,write_csf='rcsfmr.inp')
-        zeros_6s5d= Rcsfgenerate('Kr',['4d(10,i)5s(2,i)5p(6,i)4f(14,*)5d(1,*)6s(1,*)'],active_set=[6,5,5,4],jlower=0,jhigher=8,exc=0,write_csf='rcsfmr.inp')
-        zeros_even = zeros_6s2 + zeros_6s5d
+        zeros_6s2 = Rcsfgenerate('Ar',[AR_PREFIX + '4f(14,*)6s(2,*)'],active_set=[6,6,6,5],jlower=0,jhigher=2,exc=0,write_csf='rcsfmr.inp')
+        zeros_6s5d= Rcsfgenerate('Ar',[AR_PREFIX + '4f(14,*)5d(1,*)6s(1,*)'],active_set=[6,5,5,4],jlower=0,jhigher=8,exc=0,write_csf='rcsfmr.inp')
+        zeros_6p2= Rcsfgenerate('Ar',[AR_PREFIX + '4f(14,*)6p(2,*)'],active_set=[5,6,4,4],jlower=0,jhigher=6,exc=0,write_csf='rcsfmr.inp')
+        zeros_even = zeros_6s2 + zeros_6s5d #+ zeros_6p2
 
-        zeros_6s6p= Rcsfgenerate('Kr',['4d(10,i)5s(2,i)5p(6,i)4f(14,*)6s(1,*)6p(1,*)',],active_set=[6,6,4,4],jlower=0,jhigher=4,exc=0,write_csf='rcsfmr.inp')
-        zeros_4f5d= Rcsfgenerate('Kr',['4d(10,i)5s(2,i)5p(6,i)4f(13,*)5d(1,*)6s(2,*)',],active_set=[6,5,5,4],jlower=0,jhigher=14,exc=0,write_csf='rcsfmr.inp')
+        zeros_6s6p= Rcsfgenerate('Ar',[AR_PREFIX + '4f(14,*)6s(1,*)6p(1,*)',],active_set=[6,6,4,4],jlower=0,jhigher=4,exc=0,write_csf='rcsfmr.inp')
+        zeros_4f5d= Rcsfgenerate('Ar',[AR_PREFIX + '4f(13,*)5d(1,*)6s(2,*)',],active_set=[6,5,5,4],jlower=0,jhigher=14,exc=0,write_csf='rcsfmr.inp')
         zeros_odd = zeros_6s6p + zeros_4f5d
         multireference = zeros_odd + zeros_even
         return multireference
@@ -114,25 +117,26 @@ def full_calculation(calc_dir,active_set,exc,n_open):
     def ci_expansion_open_core(active_set,exc,write_csf,n_open = 6):
         # Only includes CV and VV!
         if n_open == 4:
-            prefix = '4d(10,9)5s(2,1)5p(6,5)'
+            prefix = '3d(10,c)4s(2,*)4p(6,*)4d(10,*)5s(2,*)5p(6,*)'
         if n_open == 5:
-            prefix = '4d(10,i)5s(2,1)5p(6,5)'
+            prefix = '3d(10,c)4s(2,i)4p(6,i)4d(10,i)5s(2,*)5p(6,*)'
         if n_open == 6:
-            prefix = '4d(10,i)5s(2,i)5p(6,i)'
-        exp_6s2 = Rcsfgenerate('Kr',[prefix +  '4f(14,1)6s(2,1)'],active_set=active_set,jlower=0,jhigher=2,exc=exc,write_csf=write_csf)
-        exp_4f5d = Rcsfgenerate('Kr',[prefix + '4f(14,1)5d(1,1)6s(1,1)'],active_set=active_set,jlower=0,jhigher=10,exc=exc,write_csf=write_csf)
-        exp_even = exp_6s2 + exp_4f5d
+            prefix = '3d(10,c)4s(2,i)4p(6,i)4d(10,i)5s(2,i)5p(6,i)'
+        exp_6s2 = Rcsfgenerate('Ar',[prefix +  '4f(14,1)6s(2,1)'],active_set=active_set,jlower=0,jhigher=2,exc=exc,write_csf=write_csf)
+        exp_4f5d = Rcsfgenerate('Ar',[prefix + '4f(14,1)5d(1,1)6s(1,1)'],active_set=active_set,jlower=0,jhigher=10,exc=exc,write_csf=write_csf)
+        zeros_6p2= Rcsfgenerate('Ar',[prefix + '4f(14,i)6p(2,i)'],active_set=[5,6,4,4],jlower=exc,jhigher=6,exc=0,write_csf='rcsfmr.inp')
+        exp_even = exp_6s2 + exp_4f5d #+ zeros_6p2
 
-        exp_6s6p= Rcsfgenerate('Kr',[prefix + '4f(14,1)6s(1,1)6p(1,1)'],active_set=active_set,jlower=0,jhigher=6 ,exc=exc,write_csf=write_csf)
-        exp_4f5d= Rcsfgenerate('Kr',[prefix + '4f(13,1)5d(1,1)6s(2,1)'],active_set=active_set,jlower=0,jhigher=16,exc=exc,write_csf=write_csf)
+        exp_6s6p= Rcsfgenerate('Ar',[prefix + '4f(14,1)6s(1,1)6p(1,1)'],active_set=active_set,jlower=0,jhigher=6 ,exc=exc,write_csf=write_csf)
+        exp_4f5d= Rcsfgenerate('Ar',[prefix + '4f(13,1)5d(1,1)6s(2,1)'],active_set=active_set,jlower=0,jhigher=16,exc=exc,write_csf=write_csf)
         exp_odd = exp_6s6p + exp_4f5d
         exp = exp_even + exp_odd
         return exp
 
 
-    def run_ci(calc_dir,active_set,exc,n_open):
+    indices_mr = [[1],[1],[1],[1,2],[1],[1,2],[1],[1,2],[1],[1],[1],[1],[1],[1],[1]]
+    def gen_expansion(calc_dir,active_set,exc,n_open):
         initialize(workdir=calc_dir,clist=clistordering)
-        indices_mr = [[1],[1],[1],[1,2],[1],[1,2],[1],[1,2],[1],[1],[1],[1],[1],[1],[1]]
         gen_multireference().execute(calc_dir)
         cmds = [
              Rnucleus(Z=70,A=172,neutralMass=171.936378,I=0,NDM=0,NQM=0),
@@ -145,22 +149,23 @@ def full_calculation(calc_dir,active_set,exc,n_open):
                                    fallback='Thomas-Fermi'),
              Rmcdhf(indices_mr,orbs=[],specorbs=[],weighting_method='Standard',runs=1000),
              Rsave('safronova'),
-             Rci(calc_name='safronova',
-                 include_transverse=True,
-                 modify_freq=True,
-                 scale_factor='1.d-6',
-                 include_vacpol = True,
-                 include_nms= False,
-                 include_sms = False,
-                 est_self_energy= True,
-                 largest_n = 8,
-                 asfidx = indices_mr),
-             Rsave('safronova'),
              ]
         return [cmd.execute(workdir=calc_dir) for cmd in cmds]
-
-    run_ci(dir_ci,active_set,exc,n_open)
+    gen_expansion(dir_ci,active_set,exc,n_open)
+    input('Expansion OK?')
+    Rci(calc_name='safronova',
+         include_transverse=True,
+         modify_freq=True,
+         scale_factor='1.d-6',
+         include_vacpol = True,
+         include_nms= False,
+         include_sms = False,
+         est_self_energy= True,
+         largest_n = 8,
+         asfidx = indices_mr).execute(workdir = dir_ci)
+    Rsave('safronova').execute(workdir = dir_ci)
     Rmixextract(calc_name = 'safronova',use_ci = True,tolerance = 0.05, sort = True, write_csf = 'rmix_5e-2.out').execute(dir_ci)
+    Rlevels('safronova.cm').execute(dir_ci)
     #  run_ci(calc_dir = dir_cis[0],active_set = [7,7,6,5], exc = 1,n_open = 6)
     #  run_ci(calc_dir = dir_cis[1],active_set = [8,8,7,6], exc = 1,n_open = 6)
     #  run_ci(calc_dir = dir_cis[2],active_set = [9,9,8,7], exc = 1,n_open = 6)
