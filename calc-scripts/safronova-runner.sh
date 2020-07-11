347dc27dd795468c453801910d2ea863feae3877
Calvin Leung <calvinl@mit.edu>
2020-07-10 17:30:46 -0700

    added safronova-runner and 6p2 in safronova.py exp+mr

---
+++ calc-scripts/safronova-runner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python safronova.py --maxn 8 8 8 8 8 --coren 6 --excitations 1
+python safronova.py --maxn 9 9 9 9 9 --coren 6 --excitations 1
+python safronova.py --maxn 8 8 8 8 8 --coren 5 --excitations 1
+python safronova.py --maxn 9 9 9 9 9 --coren 5 --excitations 1
+python safronova.py --maxn 8 8 8 8 8 --coren 4 --excitations 1
+python safronova.py --maxn 9 9 9 9 9 --coren 4 --excitations 1
