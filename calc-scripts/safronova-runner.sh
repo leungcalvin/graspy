4e0e5abb85442f8a9d9e55243ebd24046b238c1f
Calvin Leung <calvinl@mit.edu>
2020-07-12 01:09:15 -0700

    runner script

--- calc-scripts/safronova-runner.sh
+++ calc-scripts/safronova-runner.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-python safronova.py --maxn 8 8 8 8 8 --coren 6 --excitations 1
-python safronova.py --maxn 9 9 9 9 9 --coren 6 --excitations 1
-python safronova.py --maxn 8 8 8 8 8 --coren 5 --excitations 1
-python safronova.py --maxn 9 9 9 9 9 --coren 5 --excitations 1
-python safronova.py --maxn 8 8 8 8 8 --coren 4 --excitations 1
-python safronova.py --maxn 9 9 9 9 9 --coren 4 --excitations 1
+# rm -r /home/calvin/graspy/calc-outputs/safronova6p2/*
+#python /home/calvin/graspy/calc-scripts/safronova.py --maxn 8 8 8 8 8 --coren 6 6 5 --excitations 2 --dir /home/calvin/graspy/calc-outputs/safronova6p2
+python /home/calvin/graspy/calc-scripts/safronova.py --maxn 8 8 8 8 8 --coren 6 5 5 --excitations 2 --dir /home/calvin/graspy/calc-outputs/safronova6p2
+#python /home/calvin/graspy/calc-scripts/safronova.py --maxn 8 8 8 8 8 --coren 5 6 5 --excitations 2 --dir /home/calvin/graspy/calc-outputs/safronova6p2
+python /home/calvin/graspy/calc-scripts/safronova.py --maxn 8 8 8 8 8 --coren 5 5 5 --excitations 2 --dir /home/calvin/graspy/calc-outputs/safronova6p2
+python /home/calvin/graspy/calc-scripts/safronova.py --maxn 8 8 8 8 8 --coren 5 5 4 --excitations 2 --dir /home/calvin/graspy/calc-outputs/safronova6p2
+#python /home/calvin/graspy/calc-scripts/safronova.py --maxn 8 8 8 8 8 --coren 5 4 4 --excitations 2 --dir /home/calvin/graspy/calc-outputs/safronova6p2
+python /home/calvin/graspy/calc-scripts/safronova.py --maxn 8 8 8 8 8 --coren 4 4 5 --excitations 2 --dir /home/calvin/graspy/calc-outputs/safronova6p2
+python /home/calvin/graspy/calc-scripts/safronova.py --maxn 8 8 8 8 8 --coren 4 4 4 --excitations 2 --dir /home/calvin/graspy/calc-outputs/safronova6p2
+
+#python /home/calvin/graspy/calc-scripts/safronova.py --maxn 12 12 12 12 12 --coren 6 5 5 --excitations 1 --dir /home/calvin/graspy/calc-outputs/safronova6p2
+#python /home/calvin/graspy/calc-scripts/safronova.py --maxn 12 12 12 12 12 --coren 5 6 5 --excitations 1 --dir /home/calvin/graspy/calc-outputs/safronova6p2
+#python /home/calvin/graspy/calc-scripts/safronova.py --maxn 12 12 12 12 12 --coren 5 5 5 --excitations 1 --dir /home/calvin/graspy/calc-outputs/safronova6p2
+#python /home/calvin/graspy/calc-scripts/safronova.py --maxn 12 12 12 12 12 --coren 5 5 4 --excitations 1 --dir /home/calvin/graspy/calc-outputs/safronova6p2
+#python /home/calvin/graspy/calc-scripts/safronova.py --maxn 12 12 12 12 12 --coren 5 4 4 --excitations 1 --dir /home/calvin/graspy/calc-outputs/safronova6p2
+#python /home/calvin/graspy/calc-scripts/safronova.py --maxn 12 12 12 12 12 --coren 4 4 5 --excitations 1 --dir /home/calvin/graspy/calc-outputs/safronova6p2
+#python /home/calvin/graspy/calc-scripts/safronova.py --maxn 10 10 10 10 10 --coren 4 4 4 --excitations 1 --dir /home/calvin/graspy/calc-outputs/safronova6p2
+#python /home/calvin/graspy/calc-scripts/safronova.py --maxn 8 8 8 8 8 --coren 4 4 4 --excitations 2 --dir /home/calvin/graspy/calc-outputs/safronova6p2
+
