056b3fc62496402039b278b041a558595f0156b7
laasya <laasya@laasyas-MacBook-Air.local>
2020-08-31 12:35:53 -0700

    Added command line options for principal quantum number and excitations. Makes unique output dirs based on them.

--- radium2.py
+++ radium2.py
@@ -3,7 +3,27 @@ import sys
 from shutil import copyfile
 from os import path
 
-testdir = './calc-outputs/radium2out'
+# default n (open core orbitals) and excitations.
+princ_q_num = 10
+excitations = 1
+
+if len(sys.argv) > 1 and sys.argv[1] != None :
+    princ_q_num = int(sys.argv[1])
+
+if len(sys.argv) > 2 and sys.argv[2] != None :
+    excitations = int(sys.argv[2])
+
+print("Running for n =", princ_q_num )
+print("Running with Exications =", excitations )
+
+
+
+#testdir = './calc-outputs/radium2out'
+testdir = './calc-outputs/radium_' + str(princ_q_num) + '_' + str(excitations)
+print("Using testdir =", testdir)
+
+
+
 initialize(workdir=testdir)
 
 def gen_nucleus(calc_dir):
@@ -21,12 +41,14 @@ def estimate_wavefunctions(calc_dir,previous_rwfn = None,fallback='Thomas-Fermi'
     
 
 def run_hartree_fock_save(calc_dir,grid = None,orbs = ['*'],specorbs = ['*'],integration_method = None,calc_name = None):
-    out = Rmcdhf(asfidx = [[1],[1],[1],[1,2],[1,2],[1],[1]],orbs = orbs, specorbs = specorbs, runs = 20000, weighting_method = 'Standard',integration_method = integration_method).execute(workdir = calc_dir)
+    out = Rmcdhf(asfidx = [[1],[1],[1],[1,2],[1,2],[1,2,3,4,5],[1],[1,2,3],[1]],orbs = orbs, specorbs = specorbs, runs = 20000, weighting_method = 'Standard',integration_method = integration_method).execute(workdir = calc_dir)
     Rsave(calc_name).execute(workdir = calc_dir)
     return calc_name + '.w'
     #else:
         #return out
 
+#[1],[1],[1],[1,2],[1,2],[1],[1]
+#[1],[1],[1,2],[1,2],[1,2,3,4,5],[1],[1,2,3],[1]
 gen_nucleus(testdir)
 
 ref_7s = Rcsfgenerate(core='Rn',ordering = 'Default',
@@ -41,8 +63,12 @@ ref6_d = Rcsfgenerate(core='Rn',ordering = 'Default',
             csflist=['7s(1,i)6d(1,i)'],
             active_set = [7,6,6,4],
             jlower=2,jhigher=6,exc=0,write_csf= 'rcsfmr.inp')
+ref_7p_6d = Rcsfgenerate(core='Rn',ordering = 'Default',
+            csflist=['7p(1,i)6d(1,i)'],
+            active_set = [6,7,6,4],
+            jlower=4,jhigher=8,exc=0,write_csf= 'rcsfmr.inp')
             
-mr = ref_7p + ref_7s + ref6_d
+mr = ref_7p + ref_7s + ref6_d + ref_7p_6d
 mr.execute(workdir=testdir)
 
 angular_integration(testdir)
@@ -107,29 +133,48 @@ estimate_wavefunctions(testdir,old_rwfn)
 old_rwfn = run_hartree_fock_save(testdir, grid=None,orbs=orb_list[6], specorbs = ['*'],integration_method = 3,calc_name = 'seventh')
 print("old_rwfn=",old_rwfn)
 
+# set the right active sets based on n
+if princ_q_num == 8:
+    a_set = [8,8,8,8,8]
+elif princ_q_num == 10:
+    a_set = [10,10,10,10,10]
+elif princ_q_num == 11:
+    a_set = [11,11,11,11,11,11]
+elif princ_q_num == 12:
+    a_set = [12,12,12,12,12,12]
 
-ref_7s_e = Rcsfgenerate(core='Rn',ordering = 'Default',
-            csflist=['7s(2,*)'],
-            active_set=[8,8,8,8,8],
-            jlower=0,jhigher=0,exc=1, write_csf= 'rcsf.inp')
-ref_7p_e = Rcsfgenerate(core='Rn',ordering = 'Default',
-            csflist=['7s(1,*)7p(1,*)'],
-            active_set = [8,8,8,8,8],
-            jlower=0,jhigher=4,exc=1,write_csf= 'rcsf.inp')
-ref6_d_e = Rcsfgenerate(core='Rn',ordering = 'Default',
-            csflist=['7s(1,*)6d(1,*)'],
-            active_set = [8,8,8,8,8],
-            jlower=2,jhigher=6,exc=1,write_csf= 'rcsf.inp')
-            
-mr_e = ref_7p_e + ref_7s_e + ref6_d_e
+ref_7s_e = Rcsfgenerate(core='Xe',ordering = 'Default',
+            csflist=['4f(14,i)5d(10,9)6s(2,1)6p(6,5)7s(2,*)'],
+            #active_set=[10,10,10,10,10],
+            active_set=a_set,
+            jlower=0,jhigher=0,exc=excitations, write_csf= 'rcsf.inp')
+ref_7p_e = Rcsfgenerate(core='Xe',ordering = 'Default',
+            csflist=['4f(14,i)5d(10,9)6s(2,1)6p(6,5)7s(1,*)7p(1,*)'],
+            #active_set = [10,10,10,10,10],
+            active_set=a_set,
+            jlower=0,jhigher=4,exc=excitations,write_csf= 'rcsf.inp')
+ref6_d_e = Rcsfgenerate(core='Xe',ordering = 'Default',
+            csflist=['4f(14,i)5d(10,9)6s(2,1)6p(6,5)7s(1,*)6d(1,*)'],
+            #active_set = [10,10,10,10,10],
+            active_set=a_set,
+            jlower=2,jhigher=6,exc=excitations,write_csf= 'rcsf.inp')
+ref_7p_6d_e = Rcsfgenerate(core='Xe',ordering = 'Default',
+            csflist=['4f(14,i)5d(10,9)6s(2,1)6p(6,5)7s(2,1)7p(1,*)6d(1,*)'],
+            #active_set = [10,10,10,10,10],
+            active_set=a_set,
+            jlower=4,jhigher=8,exc=excitations,write_csf= 'rcsf.inp')
+
+mr_e = ref_7p_e + ref_7s_e + ref6_d_e + ref_7p_6d_e
 mr_e.execute(workdir=testdir)
 
 angular_integration(testdir)
 estimate_wavefunctions(testdir, old_rwfn, fallback='Screened Hydrogenic')
 old_rwfn = run_hartree_fock_save(testdir, grid=None,orbs=[], specorbs = [],integration_method = 3,calc_name = 'eighth')
 print("old_rwfn=",old_rwfn)
+# saves eight.w and eight.c into radium.w and radium.c respectively
 Rsave('radium').execute(workdir = testdir)
 
+# RCI uses radium.c and radium.w and produces radium.cm, radium.clog, radium.m,rci.res,  radium.csum...
 Rci(calc_name= 'radium',
     include_transverse=True,
     modify_freq=True,
@@ -139,7 +184,8 @@ Rci(calc_name= 'radium',
     include_sms = False,
     est_self_energy= True,
     largest_n = 8,
-    asfidx = [[1],[1],[1],[1,2],[1,2],[1],[1]]).execute(workdir = testdir)
+    asfidx = [[1],[1],[1],[1,2],[1,2],[1,2,3,4,5],[1],[1,2,3],[1]]).execute(workdir = testdir)
 
 #Rsave('radiumCI').execute(workdir = testdir)
-#Rlevels('radiumCI').execute(workdir = testdir)
+# rlevels uses radium.cm to work. It is not happy with just radium
+Rlevels('radium.cm').execute(workdir = testdir)
